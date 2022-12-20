d7a28ab51ef3972a4f05e1e37dfc1e0975bec708
Den Gutman <dengutman@gmail.ru>
2022-12-20 18:27:06 +0200

    20.12dogs

--- Class Works/CW#06/src/dogs.js
+++ Class Works/CW#06/src/dogs.js
@@ -1,17 +1,19 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
-const detailsContainer = document.querySelector(".details-container")
+const detailsContainer = document.querySelector(".details-container");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor")
 const mainElement = document.querySelector(".main-class");
 const hideButtonElement = document.getElementById("hide-button");
+const audio = document.querySelector('audio');
 const HIDDEN = "hidden";
 const IS_POINT = "is-point";
 function showDetails() {
     mainElement.classList.remove(HIDDEN);
     detailsContainer.classList.add(IS_POINT);
-    setTimeout(function (){
-       detailsContainer.classList.remove(IS_POINT)        
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
     })
+
 }
 function hideDetails() {
     mainElement.classList.add(HIDDEN);
@@ -22,6 +24,11 @@ function setDetails(anchor) {
     showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
     const color = anchor.getAttribute("data-text-color");
+    audio.src = anchor.getAttribute("data-details-bark");
+    audio.play();
+    setTimeout(function () {
+        audio.pause();
+    }, 3000)
     if (color) {
         detailsTitle.style.color = color;
     } else {
